d92d5891e1b54fdc5ba7f0d3aa0eb32c6dcbc0a4
Sh3mm <antoinearobillard@gmail.com>
2017-11-01 17:57:41 -0400

    added the SumOfProduct fonction and all the used fonction

--- possibility gen.py
+++ possibility gen.py
@@ -24,3 +24,43 @@ def allPossibilities (nbOfVariables):
 
     return arrayFinal
 
+def sumOfProducts (answersList, nbOfVariables):
+    sumOfProduct = "F = "
+    for answers in answersList:
+        if answers[nbOfVariables] == 1:
+            product = returnProduct(makeListOf1(answers), nbOfVariables)
+            sumOfProduct += "(" + str(product) + ")"
+
+    return sumOfProduct
+
+def makeListOf1 (answers):
+    listOf1 = []
+    placeInList = 0
+    for x in answers[:-1]:
+        if x == 1:
+            listOf1.append(placeInList)
+        placeInList += 1
+    return listOf1
+
+def returnProduct(listof1, nbofVariables):
+    product = ""
+    for i in range(nbofVariables):
+        check = False
+        for j in listof1:
+            if i == j:
+                check = True
+                break
+        if check == True:
+            product += str(1+i)
+        else:
+            product += str(1 + i) + "'"
+        if i != (nbofVariables-1):
+            product += "+"
+
+    return product
+
+
+
+def display ():
+    #todo make shit display
+    JUST_FOR_COMPILING
